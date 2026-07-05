bbc9998e7aa3720062f3d9684db1707343c72c2f
MoNasser <mn77865@gmail.com>
2026-07-05 14:42:04 +0300

    feat: implement responsive session body widgets for mobile and tablet layouts

--- lib/features/sessions/presentation/widgets/tablet/tablet_session_body.dart
+++ lib/features/sessions/presentation/widgets/tablet/tablet_session_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:local_erp_system/features/sessions/presentation/widgets/sessions_list_view.dart';
 
 import '../../../../../core/utils/gaps.dart';
+import '../../cubits/cubit/session_cubit.dart';
 import '../main_session_data_holder.dart';
 import '../session_items_list/session_item_add_button.dart';
 import '../session_items_list/session_items_header_row.dart';
@@ -11,17 +14,29 @@ class TabletSessionBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        sliverGapHFix(20),
-        const MainSessionDataHolder(),
-        sliverGapHFix(12),
-        const SessionItemsHeaderRow(),
-        const SessionItemsList(),
-        sliverGapHFix(8),
-        const SessionItemAddButton(),
-        sliverGapHFix(20),
-      ],
+    return BlocSelector<SessionCubit, SessionState, int>(
+      selector: (state) => state.currentTapIndex,
+      builder: (context, currentTapIndex) {
+        switch (currentTapIndex) {
+          case 0:
+            return CustomScrollView(
+              slivers: [
+                sliverGapHFix(20),
+                const MainSessionDataHolder(),
+                sliverGapHFix(12),
+                const SessionItemsHeaderRow(),
+                const SessionItemsList(),
+                sliverGapHFix(8),
+                const SessionItemAddButton(),
+                sliverGapHFix(20),
+              ],
+            );
+          case 1:
+            return const SessionsListView();
+          default:
+            return const SizedBox.shrink();
+        }
+      },
     );
   }
 }
